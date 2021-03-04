727be52da230639efb4ad5cf227126215abcb408
Blaise Liu <liujianqing@msn.com>
2021-03-04 16:15:19 -0500

    core3.1 app w. authentication

---
+++ QuickBookReferenceSolution/apps/QuickBookReference.Core31/Controllers/CallbackController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace QuickBookReference.Core31.Controllers
+{
+    public class CallbackController:Controller
+    {
+        private readonly ILogger<CallbackController> _logger;
+
+        public CallbackController(ILogger<CallbackController> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            
+            string state = Request.Query["state"];
+            string code = Request.Query["code"];
+            string realmId = Request.Query["realmId"];
+
+            var isValidState = state.Equals(HomeController.auth2Client.CSRFToken, StringComparison.Ordinal);
+
+            ViewBag.State = $"{state} ({(isValidState?"valid":"invalid")})";
+
+            //string code = queryDictionary["code"].ToString() ?? "none";
+            //string realmId = queryDictionary["realmId"].ToString() ?? "none";
+            await GetAuthTokensAsync(code, realmId);
+
+            //ViewBag.Error = queryDictionary["error"].ToString() ?? "none";
+
+            return RedirectToAction("Tokens", "Home");
+        }
+
+        private async Task GetAuthTokensAsync(string code, string realmId)
+        {
+            //if (realmId != null)
+            //{
+            //    HttpContext.Session.SetString("realmId", realmId);
+            //}
+
+            var tokenResponse = await HomeController.auth2Client.GetBearerTokenAsync(code);
+
+            var claims = new List<Claim>();
+
+            claims.Add(new Claim("realmId", realmId));
+            //if (HttpContext.Session.GetString("realmId") != null)
+            //{
+            //    claims.Add(new Claim("realmId", HttpContext.Session.GetString("realmId")));
+            //}
+
+            if (!string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
+            {
+                claims.Add(new Claim("access_token", tokenResponse.AccessToken));
+                claims.Add(new Claim("access_token_expires_at", (DateTime.Now.AddSeconds(tokenResponse.AccessTokenExpiresIn)).ToString()));
+            }
+
+            if (!string.IsNullOrWhiteSpace(tokenResponse.RefreshToken))
+            {
+                claims.Add(new Claim("refresh_token", tokenResponse.RefreshToken));
+                claims.Add(new Claim("refresh_token_expires_at", (DateTime.Now.AddSeconds(tokenResponse.RefreshTokenExpiresIn)).ToString()));
+            }
+            var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+            await
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity));
+        }
+    }
+}
---
+++ QuickBookReferenceSolution/apps/QuickBookReference.Core31/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuickBookReference.Core31.Models;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using Intuit.Ipp.Core;
+using Intuit.Ipp.Data;
+using Intuit.Ipp.OAuth2PlatformClient;
+using Intuit.Ipp.QueryFilter;
+using Intuit.Ipp.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace QuickBookReference.Core31.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public static OAuth2Client auth2Client;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            var clientId = config.GetValue<string>("QuickBook:ClientId");
+            var clientSecret = config.GetValue<string>("QuickBook:ClientSecret");
+            var redirectUrl = config.GetValue<string>("QuickBook:RedirectUrl");
+            var environment = config.GetValue<string>("QuickBook:Environment");
+
+            auth2Client = new OAuth2Client(clientId, clientSecret, redirectUrl, environment);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult InitiateAuth()
+        {
+            var scopes = new List<OidcScopes> { OidcScopes.Accounting };
+            var authorizeUrl = auth2Client.GetAuthorizationURL(scopes);
+            return Redirect(authorizeUrl);
+        }
+        [Authorize]
+        public IActionResult Tokens()
+        {
+            return View(User.Claims);
+        }
+        public ActionResult ApiCallService()
+        {
+            if (HttpContext.Session.GetString("realmId") != null)
+            {
+                var realmId = HttpContext.Session.GetString("realmId");
+                try
+                {
+                    var principal = User;
+                    var oauthValidator = new OAuth2RequestValidator(principal.FindFirst("access_token").Value);
+
+                    // Create a ServiceContext with Auth tokens and realmId
+                    var serviceContext = new ServiceContext(realmId, IntuitServicesType.QBO, oauthValidator);
+                    serviceContext.IppConfiguration.MinorVersion.Qbo = "23";
+
+                    // Create a QuickBooks QueryService using ServiceContext
+                    var querySvc = new QueryService<CompanyInfo>(serviceContext);
+                    var companyInfo = querySvc.ExecuteIdsQuery("SELECT * FROM CompanyInfo").FirstOrDefault();
+
+                    var output = "Company Name: " + companyInfo?.CompanyName + " Company Address: " + 
+                                 companyInfo?.CompanyAddr.Line1 + ", " + 
+                                 companyInfo?.CompanyAddr.City + ", " + 
+                                 companyInfo?.CompanyAddr.Country + " " + 
+                                 companyInfo?.CompanyAddr.PostalCode;
+                    return View("ApiCallService", "QBO API call Successful!! Response: " + output);
+                }
+                catch (Exception ex)
+                {
+                    return View("ApiCallService", "QBO API call Failed!" + " Error message: " + ex.Message);
+                }
+            }
+            else
+            {
+                return View("ApiCallService", "QBO API call Failed!");
+            }
+        }
+    }
+}
